dc6cc38dfa7552facb472bc26564fb2d963de370
Jibesh Kumar Panda <99865910+Jibesh04@users.noreply.github.com>
2022-07-09 17:49:45 +0530

    Added PrimeCtr() and EvenCtr()

--- GCDLCM.C
+++ GCDLCM.C
@@ -8,5 +8,19 @@ int main()
  int n1, n2;
  clrscr();
  printf("Enter the two numbers : ");
- scanf("%d %d". &n1. &n2);
- printf("")
+ scanf("%d %d", &n1, &n2);
+ printf("Greatest Common Divisor : %d", gcd(n1, n2));
+ printf("\nLeast Common Multiple : %d", lcm(n1, n2));
+ getch();
+ return 0;
+}
+int gcd(int a, int b)
+{
+ if(b == 0)
+  return a;
+ return gcd(b, a%b);
+}
+int lcm(int a, int b)
+{
+ return ((a * b)/gcd(a, b));
+}
---
+++ PREVCTR.C
@@ -0,0 +1,46 @@
+// 7. Write a program using recursive function to count all the prime
+// numbers, even numbers between p and q (both inclusive)
+#include<stdio.h>
+#include<conio.h>
+int isPrime(int);
+int primectr(int, int);
+int evenctr(int, int);
+int main()
+{
+ int p, q;
+ clrscr();
+ printf("Enter the two numbers : ");
+ scanf("%d %d", &p, &q);
+ printf("Couunt of Prime numbers : %d", primectr(p, q));
+ printf("\nCount of Even numbers : %d", evenctr(p, q));
+ getch();
+ return 0;
+}
+int isPrime(int n)
+{
+ int i, ctr = 0;
+ for(i = 1; i <= n/2; i++)
+ {
+  if(n%i == 0)
+   ctr++;
+ }
+ if(ctr == 1)
+  return 1;
+ return 0;
+}
+int primectr(int a, int b)
+{
+ if(a == b + 1)
+  return 0;
+ if(isPrime(a) == 1)
+  return (1 + primectr(a+1, b));
+ return primectr(a+1, b);
+}
+int evenctr(int a, int b)
+{
+ if(a == b + 1)
+  return 0;
+ if(a%2 == 0)
+  return (1 + evenctr(a+1, b));
+ return evenctr(a+1, b);
+}
